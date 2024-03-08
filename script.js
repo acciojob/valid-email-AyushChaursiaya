cc578d751beceaac46117503d19e5eace8739f56
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-08 10:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function validEmail(str) {
   //your JS code here.
-	const emailRegex = /^[a-zA-Z0-9.,_%+-]+@[a-zA-A-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  // Regular expression to match email format
+	// const emailRegex = /^[a-zA-Z0-9.,_%+-]+@[a-zA-A-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  // Check if the input string matches the email format
+	// return emailRegex.test(str);
+const emailRegex = /[a-zA-Z0-9.,_-%-+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 	return emailRegex.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
